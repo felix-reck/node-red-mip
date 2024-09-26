8a057d69e3eb49ceee6f9360f2eac3d0ead9a01d
felix-reck <>
2024-09-26 13:58:44 +0200

    new example and input validation

--- config/remote-server.js
+++ config/remote-server.js
@@ -6,13 +6,13 @@ module.exports = function(RED) {
         this.url = n.url;
         this.user = n.user;
         this.password = this.credentials.password; // aus credentials store
-        this.accessId = n.accessId;
+        this.accessId = String(n.accessId).padStart(8, '0'); //
         this.sslVerify = n.sslVerify
         
     }
-    RED.nodes.registerType("remote-server",RemoteServerNode,{
+    RED.nodes.registerType('remote-server',RemoteServerNode,{
         credentials: {
-            password: {type:"password"}
+            password: {type:'password'}
         }
     });
 }
